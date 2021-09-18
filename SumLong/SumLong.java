5e89bdd6d381be9bd37b033682af28aa2bc648f8
Maria Okorochkova <84291094+maladetska@users.noreply.github.com>
2021-09-18 10:13:26 +0300

    Update SumLong.java

--- SumLong/SumLong.java
+++ SumLong/SumLong.java
@@ -1,18 +1,18 @@
 public class SumLong {
-    public static void main(String[] args) {
-        long sum = 0;
-        for (String currArg: args) { 
-            for (int i = 0; i < currArg.length(); i++) {
-		int ctrlInd = i;
-		while (i < currArg.length() && !Character.isWhitespace(currArg.charAt(i))) {
-		    i++;
+	public static void main(String[] args) {
+		long sum = 0;
+		for (String currArg: args) { 
+			for (int i = 0; i < currArg.length(); i++) {
+				int ctrlInd = i;
+				while (i < currArg.length() && !Character.isWhitespace(currArg.charAt(i))) {
+					i++;
+				}
+				String currNum = currArg.substring(ctrlInd, i);
+				if (!currNum.isEmpty()) {
+					sum += Long.parseLong(currNum);
+				}
+			}
 		}
-		String currNum = currArg.substring(ctrlInd, i);
-		if (!currNum.isEmpty()) {
-		    sum += Long.parseLong(currNum);
-		}
-	    }
+		System.out.println(sum);
 	}
-	System.out.println(sum);
-    }
 }
