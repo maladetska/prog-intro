ec7281b37e5d1bb62f7d4d2a8a3dcfc5a1b6bc6e
maladetska <okorochkova02@gmail.com>
2022-07-16 22:23:05 +0300

    HW 3: mod Odd2

---
+++ java-solutions/reverse/ReverseOdd2.java
@@ -0,0 +1,70 @@
+import java.util.Scanner;
+
+public class ReverseOdd2 {
+    private static final int SIZE = 15;
+
+    static int currPos = 0;
+
+    static int[] arrayOfLines;
+    static int countOfLines;
+
+    static int[] arrayOfNumbers;
+    static int countOfNumbers;
+
+    public static int[] resizeArray(int[] array) {
+        int size = array.length;
+        int[] newArray = new int[size * 2];
+        System.arraycopy(array, 0, newArray, 0, size);
+
+        return newArray;
+    }
+
+    public static boolean sizeCheck(int[] array, int count) {
+        return count >= array.length;
+    }
+
+    public static void printNumber(int indexLine) {
+        for (int i = 0; i < arrayOfLines[indexLine]; i++) {
+            System.out.print(arrayOfNumbers[--currPos] + " ");
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        arrayOfNumbers = new int[SIZE];
+        arrayOfLines = new int[SIZE];
+        countOfLines = 0;
+
+        while (scanner.hasNextLine()) {
+            Scanner scannerLine = new Scanner(scanner.nextLine());
+
+            if (sizeCheck(arrayOfLines, countOfLines)) {
+                arrayOfLines = resizeArray(arrayOfLines);
+            }
+
+            countOfNumbers = 0;
+            while (scannerLine.hasNextInt()) {
+                if (sizeCheck(arrayOfNumbers, currPos)) {
+                    arrayOfNumbers = resizeArray(arrayOfNumbers);
+                }
+
+                int number = scannerLine.nextInt();
+                if ((countOfLines + countOfNumbers) % 2 != 0) {
+                    arrayOfNumbers[currPos++] = number;
+                    arrayOfLines[countOfLines]++;
+                }
+                countOfNumbers++;
+            }
+            scannerLine.close();
+            countOfLines++;
+        }
+        scanner.close();
+
+        for (int i = countOfLines - 1; i > 0; i--) {
+            printNumber(i);
+            System.out.print("\n ");
+        }
+        printNumber(0);
+    }
+}
